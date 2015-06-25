f24a625d1fb36c3686964f9f593ccbd70265bd70
unknown <fgeorgescu@ixro-c1000-d.ixiacom.com>
2015-06-25 15:49:54 +0300

    changes to PostMe

--- PostMe/PostMe.js
+++ PostMe/PostMe.js
@@ -1,11 +1,46 @@
 	
+	Posts = new Mongo.Collection("posts");
+	
+
 	if (Meteor.isClient) 
 	{
-		angular.module("PostMe", ["angular-meteor"]);
-	}
+		angular.module('PostMe',['angular-meteor']);
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+		angular.module("PostMe").controller("postsController", ['$scope', '$meteor',
+		function($scope, $meteor)
+		{
+			$scope.posts = $meteor.collection(Posts);
+
+		}]);
+	}
+		
+	if (Meteor.isServer) 
+	{ 	
+		Meteor.startup(function()
+		{
+			var posts = [
+				{
+					'name' : "I`m in Bucharest",
+					'picture' : "Insert Picture",
+					'location': "get location"
+				},
+				{
+					'name' : "I`m in Paris",
+					'picture' : "Insert Picture",
+					'location': "get location"
+				},
+				{
+					'name' : "I`m in London",
+					'picture' : "Insert Picture",
+					'location': "get location"
+				}
+			];	
+			
+			for(var i=0; i< posts.length; i++)
+			{
+				Posts.insert({name: posts[i].name, camera: posts[i].camera, location: posts[i].location});
+			}
+		})
+		
+		
+	}

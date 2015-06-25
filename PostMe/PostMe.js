92c2ac33ba7a73a66e73f1401e189cb34000237d
unknown <fgeorgescu@robuc-400321-L.ixiacom.com>
2015-06-26 00:14:37 +0300

    new changes

--- PostMe/PostMe.js
+++ PostMe/PostMe.js
@@ -4,50 +4,67 @@
 
 	if (Meteor.isClient) 
 	{
-		angular.module('PostMe',['angular-meteor']);
+		angular.module('PostMe',['angular-meteor', 'ui.router']);
+		
+		angular.module("PostMe").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+		  function($urlRouterProvider, $stateProvider, $locationProvider)
+		  {
+				$locationProvider.html5Mode(true);
 
-		angular.module("PostMe").controller("postsController", ['$scope', '$meteor',
-		function($scope, $meteor)
-		{
-			$scope.posts = $meteor.collection(Posts);
-			
-			$scope.addPost = function(new_post)
-			{
-				//Make sure the user is Logged in before inserting a Tasks
-				// if (!Meteor.userId())
-				// {
-					// throw new Meteor.Error("not-authorized");
-				// }
-			
-				Posts.insert(
+				$stateProvider
+				
+				.state('posts', 
 				{
-					name:new_post.name,
-					selfie:new_post.selfie,
-					location:new_post.location,
-					createdAt: new Date(),
-					// owner: Meteor.userId(),
-					// username: Meteor.user().username
+					url: '/posts',
+					templateUrl: 'PostMe.ng.html',
+					controller: 'postsController'
 				});
-			},
-			
-			$scope.remove = function(post)
+				
+				$urlRouterProvider.otherwise("/posts");
+			}]);
+		
+		angular.module("PostMe").controller("postsController", ['$scope', '$meteor',
+			function($scope, $meteor)
 			{
-				$scope.posts.remove(post);
-			};
-			
-			$scope.addSelfie = function(newPostSelfie)
-			{
-				$scope.newPost.selfie = 'selfie nou';
-			};
-			
-			$scope.addLocation = function(newPostLocation)
-			{
-				$scope.newPost.location = 'location nou';
-			};
-			
-			//function that removes all posts from that user -which is logged in
+				$scope.posts = $meteor.collection(Posts);
+				
+				$scope.addPost = function(new_post)
+				{
+					//Make sure the user is Logged in before inserting a Tasks
+					// if (!Meteor.userId())
+					// {
+						// throw new Meteor.Error("not-authorized");
+					// }
+				
+					Posts.insert(
+					{
+						name:new_post.name,
+						selfie:new_post.selfie,
+						location:new_post.location,
+						createdAt: new Date(),
+						// owner: Meteor.userId(),
+						// username: Meteor.user().username
+					});
+				},
+				
+				$scope.remove = function(post)
+				{
+					$scope.posts.remove(post);
+				};
+				
+				$scope.addSelfie = function(newPostSelfie)
+				{
+					$scope.newPost.selfie = 'selfie nou';
+				};
+				
+				$scope.addLocation = function(newPostLocation)
+				{
+					$scope.newPost.location = 'location nou';
+				};
+				
+				//function that removes all posts from that user -which is logged in
 
-		}]);
+			}]);
 	
 	}
 	
@@ -58,6 +75,4 @@
 		{
 			//
 		})
-		
-		
 	}
